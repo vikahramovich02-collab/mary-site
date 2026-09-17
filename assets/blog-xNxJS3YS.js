caa6b3cb866ab174f04b0f6f49c3a679644dfc5f
Виктория <vika@MacBook-Air-Viktoria.local>
2026-09-17 17:35:16 +0300

    Деплой сайта 17.09.2026 17:35

---
+++ assets/blog-xNxJS3YS.js
@@ -0,0 +1,1 @@
+import"./Cursor-jHgVL8cb.js";import"./chrome-Ch4QNEMI.js";(function(){var e=document.querySelectorAll(`.list-tab`),t=document.querySelectorAll(`.list-card`);e.length&&e.forEach(function(n){n.addEventListener(`click`,function(){var r=n.dataset.filter;e.forEach(function(e){e.classList.toggle(`is-active`,e===n)}),t.forEach(function(e){e.hidden=r!==`all`&&e.dataset.tag!==r})})})})();
